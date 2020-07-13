982096cc9b365683495ed1d698bb0771bd0a5859
kostyaval <rebenkokk@gmail.com>
2020-07-13 18:19:34 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <canvas id="hero-lightpass" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      images: {},
+    }
+  },
+  mounted() {
+    const html = document.documentElement
+    const canvas = document.getElementById('hero-lightpass')
+    const context = canvas.getContext('2d')
+
+    const frameCount = 147
+    const currentFrame = (index) =>
+      `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index
+        .toString()
+        .padStart(4, '0')}.jpg`
+
+    const preloadImages = () => {
+      for (let i = 1; i <= frameCount; i++) {
+        const img = new Image()
+        img.src = currentFrame(i)
+        this.images[i] = img
+      }
+    }
+
+    const img = new Image()
+    img.src = currentFrame(1)
+    canvas.width = 1158
+    canvas.height = 770
+    img.onload = function () {
+      context.drawImage(img, 0, 0)
+    }
+
+    const updateImage = (index) => {
+      console.log(index, this.images[index])
+      context.drawImage(this.images[index], 0, 0)
+    }
+
+    window.addEventListener('scroll', () => {
+      const scrollTop = html.scrollTop
+      const maxScrollTop = html.scrollHeight - window.innerHeight
+      const scrollFraction = scrollTop / maxScrollTop
+      const frameIndex = Math.min(
+        frameCount - 1,
+        Math.ceil(scrollFraction * frameCount)
+      )
+
+      requestAnimationFrame(() => updateImage(frameIndex + 1))
+    })
+
+    preloadImages()
+  },
+}
+</script>
+
+<style>
+html {
+  height: 100vh;
+}
+
+body {
+  height: 200vh;
+  background: #000;
+}
+
+canvas {
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  max-width: 100vw;
+  max-height: 100vh;
+}
+</style>
